dab5dc08cd47ee49596452a96ba6e0571cee4fca
grosmastery <grosmastery@gmail.com>
2022-01-20 12:44:36 +0200

    first commit

---
+++ python_dz_18.01.py
@@ -0,0 +1,18 @@
+first = 10
+second = 30
+sum = first + second
+dif = first - second
+prod = first * second
+quo = second / first
+deg_first = first ** 2
+deg_second = second ** 2
+quo1 = second // first # выдает int число вместо float
+quo2 = second % first # выдает остаток от деления
+print(sum)
+print(dif)
+print(prod)
+print(quo)
+print(deg_first)
+print(deg_second)
+print(quo1)
+print(quo2
